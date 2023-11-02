d3f21e9666b4ff2c4615aa2c031eeb1cfadbc44e
ben-s <benjamin_seah@outlook.com>
2023-11-03 00:52:51 +0800

    streamlit update

--- plastic-resin-codes/streamlit_inference.py
+++ plastic-resin-codes/streamlit_inference.py
@@ -1,12 +1,14 @@
 #import dependencies
 import streamlit as st
 import requests
-from io import BytesIO
+from io import BytesIO 
+from PIL import Image # to open image from BytesIO object
 
+from tensorflow.keras.applications.efficientnet_v2 import preprocess_input as efficientnetv2_preprocess_input
 #############################
 
 # Server api
-api_url = "https://plastic-resin-code-r7wxdlansa-as.a.run.app"
+api_url = "http://192.168.50.24:8080"
 api_route = '/predict'
 
 # Page config
@@ -31,13 +33,27 @@ if uploaded_image is None:
 
 else: 
     st.spinner("In progress.. Please give it a few seconds..")
-    file_data = uploaded_image.read()
+    image_data = uploaded_image.read() # read file  
+    image_pil = Image.open(BytesIO(image_data)) # display image using PIL
+   
     #Send the image to the Flask API
-    response = requests.post(f'{api_url}{api_route}', files={'image': file_data})
+    response = requests.post(f'{api_url}{api_route}', files={'image': image_data})
 
     try:
+        # Check if the image has EXIF data and an orientation tag to reorientate for 
+        exif = image_pil._getexif()
+        orientation = exif.get(0x0112, 1)
+        if orientation == 3:
+            image_pil = image_pil.rotate(180, expand=True)
+        elif orientation == 6:
+            image_pil = image_pil.rotate(270, expand=True)
+        elif orientation == 8:
+            image_pil = image_pil.rotate(90, expand=True)
+            
+        #
+        #send image data and retrieve response
         prediction = response.json()['predictions'][0]
-        st.image(uploaded_image, caption= f'Uploaded image: {uploaded_image.name}', width=224)
+        st.image(image_pil, caption= f'Uploaded image: {uploaded_image.name}', width=224)
         st.write(f'Code type: **{prediction}**')
     except Exception as e:
         st.write(f'Error processing API response: {e}')
